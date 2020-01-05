06b7472794b42b8d463fd5af9cddd54f8501453c
Guilherme França <guii_real@hotmail.com>
2020-01-05 02:37:39 -0300

    All CRUD Methods created

--- api/src/controllers/ProductController.js
+++ api/src/controllers/ProductController.js
@@ -9,7 +9,7 @@ module.exports = {
   },
 
   async show(req, res) {
-    const { id } = req.params;
+    const {id} = req.params;
     const product = await Product.findById(id);
     return res.json(product);
   },
@@ -17,5 +17,21 @@ module.exports = {
   async store(req, res) {
     const product = await Product.create(req.body);
     return res.json(product);
+  },
+
+  async update(req, res) {
+    const {id} = req.params;
+
+    // O USO DO "{ new: true }" RETORNA O PRODUTO ATUALIZADO
+    // PARA A VARIÁVEL "product"
+    const product = await Product.findByIdAndUpdate(id, req.body, {new: true});
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+    return res.json({
+      deleted: true
+    });
   }
 };
