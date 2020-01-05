c22dd11457a689ee1b081b939acf2cb6466507fb
Guilherme França <guii_real@hotmail.com>
2020-01-05 02:20:55 -0300

    Configure Express, Router and Mongoose

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const requireDir = require('require-dir');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/JSHunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
