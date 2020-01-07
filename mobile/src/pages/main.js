ad30f595a29f4d8640cc30dab799272f96a102fc
Guilherme França <guii_real@hotmail.com>
2020-01-07 01:18:39 -0300

    Final Commit

--- mobile/src/pages/main.js
+++ mobile/src/pages/main.js
@@ -1,13 +1,108 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import api from '../services/api';
 
-const Main = () => {
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+
+const Main = ({navigation}) => {
+
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [productInfo, setProductInfo] = useState({});
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  const loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+    const {docs, ...productInfo} = response.data;
+    const newDocs = [...products, ...docs];
+    setProducts(newDocs);
+    setProductInfo(productInfo);
+    setPage(page);
+  };
+
+  const loadMore = () => {
+    if (page === productInfo.totalPages) return;
+    const pageNumber = page + 1;
+    loadProducts(pageNumber);
+  };
+
+  const renderItem = ({item}) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.producTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
+      <TouchableOpacity style={styles.productButton} onPress={() => {
+        navigation.navigate('Product', {product: item});
+      }}>
+        <Text style={styles.productButtonText}>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
-    <View>
-      <Text>Página Main</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={products}
+        keyExtractor={item => item._id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.list}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.1}
+      />
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FAFAFA'
+  },
+  list: {
+    padding: 20,
+  },
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20
+  },
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24
+  },
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10
+  },
+  productButtonText: {
+    fontSize: 16,
+    color: '#DA552F',
+    fontWeight: 'bold'
+  }
+
+});
 
 export default Main;
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,16 +2,26 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from './pages/main';
+import Product from './pages/product';
 
 const rootStack = createStackNavigator({
   Main: {
     screen: Main,
     navigationOptions: {
       title: 'JSHunt',
-      headerTitleStyle: {
-        textAlign: 'center',
-        alignSelf: 'center'
-      }
+      headerStyle: {
+        backgroundColor: "#DA552F"
+      },
+      headerTintColor: "#FFFFFF"
+    }
+  },
+  Product: {
+    screen: Product,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#DA552F"
+      },
+      headerTintColor: "#FFFFFF"
     }
   }
 });
